245ca74c00d7f735575d3c7ab88578f849950c57
fadi <fadi mabsaleh>
2018-05-07 11:59:38 +0300

    testing part-1

--- back/index.js
+++ back/index.js
@@ -7,4 +7,32 @@ const handleRequest = (req, res) => {
 }
 
 server.on('request', handleRequest)
-server.listen(8888, ()=> console.log('server is ready'))
+server.listen(8888, () => console.log('server is ready'))
+
+// socket testing
+
+const io = require('socket.io')(server);
+
+let globalNumber = 0
+
+io.on('connection', (socket) => {
+
+    console.log('a user connected')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('increment', () => {
+        globalNumber++
+        io.emit('number:change', globalNumber)
+    });
+
+    socket.on('decrement', () => {
+        globalNumber--
+        io.emit('number:change', globalNumber)
+    });
+
+    socket.emit('number:change', globalNumber)
+
+});
--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import io from 'socket.io-client';
 import './App.css';
 
 class App extends Component {
+
+  state = { socket: null, globalNumber: 0 }
+
+  componentDidMount() {
+    const socket = io('http://localhost:8888');
+
+    this.setState({ socket })
+
+    socket.on('number:change', (globalNumber) => {
+      this.setState({ globalNumber })
+    })
+
+  }
+
+  onIncrement = () => this.state.socket.emit('increment')
+  onDecrement = () => this.state.socket.emit('decrement')
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        hello
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
